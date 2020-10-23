4e61570c3dbceb320d9874fa6995afeded1256bd
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-23 23:47:51 +0300

    create test

---
+++ BrowserTests/BrowserLibraryTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssemblyLibrary;
+using System.Linq;
+using System.IO;
+
+namespace AssemblyLibTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private BrowserLibrary _browserLibrary;
+        private AssemblyInfo _assemblyInfo;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _browserLibrary = new BrowserLibrary();
+            var pluginsPath = Directory.GetCurrentDirectory() + "\\FakerLibrary.dll";
+            _assemblyInfo = _browserLibrary.GetResult(pluginsPath);
+        }
+
+
+        [TestMethod]
+        public void NamespaceNameTest()
+        {
+            var namespaces = _assemblyInfo.Namespaces;
+            Assert.IsTrue(namespaces.Any(n => n.NamespaceName == "FakerLibrary"));
+        }
+
+        [TestMethod]
+        public void TypesCount()
+        {
+            const int typesCountExpectedFaker = 5;
+            const int typesCountExpectedGens = 7;
+
+            var namespaces = _assemblyInfo.Namespaces;
+            var typesFaker = namespaces.First(n => n.NamespaceName == "FakerLibrary").info;
+            var typesCountActualFaker = typesFaker.Count;
+
+            var typesGenerators = namespaces.First(n => n.NamespaceName == "FakerLibrary.Generators.TypesGenerators").info;
+            var typesCountActualGenerators = typesGenerators.Count;
+
+            Assert.AreEqual(typesCountExpectedFaker, typesCountActualFaker);
+            Assert.AreEqual(typesCountExpectedGens, typesCountActualGenerators);
+        }
+
+        [TestMethod()]
+        public void MethodsCheck()
+        {
+            var namespaces = _assemblyInfo.Namespaces;
+            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").info.FirstOrDefault(t => t.TypeName == "IGenerator");
+            const string methodname = "CanGenerate";
+            const string methodSignature = "(Type)";
+
+            Assert.IsNotNull(testType);
+            Assert.IsTrue(testType.Methods.Count == 2);
+            Assert.IsTrue(testType.Methods.Any(m => m.MethodName == methodname));
+            Assert.IsTrue(testType.Methods.FirstOrDefault(m => m.MethodName == methodname)?.methodSignature == methodSignature);
+        }
+
+        [TestMethod()]
+        public void TypeCheck()
+        {
+            var namespaces = _assemblyInfo.Namespaces;
+            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").info.FirstOrDefault(t => t.TypeName == "Faker");
+            const int expectedFieldsAmounts = 4;
+            const int expectedPropertiesAmounts = 1;
+            const int expectedMethodsAmounts = 10;
+
+            Assert.IsNotNull(testType);
+            Assert.AreEqual(expectedFieldsAmounts,testType.Fields.Count);
+            Assert.AreEqual(expectedPropertiesAmounts, testType.Properties.Count);
+            Assert.AreEqual(expectedMethodsAmounts, testType.Methods.Count);
+        }
+
+
+    }
+}
---
+++ BrowserTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BrowserTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BrowserTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4e9ed8a3-bacd-420a-bcb1-792b17f6879b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
