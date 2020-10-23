61fc0c48f5d80365b5a064d52365176e2e7af948
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-23 23:45:11 +0300

    create assembly for methods and namespace

---
+++ AssemblyLibrary/AssemblyMethod.cs
@@ -0,0 +1,16 @@
+﻿namespace AssemblyLibrary
+{
+    public class AssemblyMethod
+    {
+        public string MethodName { get; set; }
+        public string methodSignature;
+        public string GetMethodInfo => MethodName + " " + methodSignature;
+
+        public AssemblyMethod(string name, string signature)
+        {
+            MethodName = name;
+            methodSignature = signature;
+        }
+
+    }
+}
---
+++ AssemblyLibrary/AssemblyNamespace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AssemblyLibrary
+{
+    public class AssemblyNamespace
+    {
+        public List<TypeInfo> info { get; set; }
+        public string NamespaceName { get; set; }
+
+        public AssemblyNamespace(string name, List<TypeInfo> types )
+        {
+            NamespaceName = name;
+            info = types;
+        }
+    }
+}
