1151808151be6ec5fcee115f7cb7b1dabbe042fa
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-23 23:44:33 +0300

    create browser library and assembly info

---
+++ AssemblyLibrary/AssemblyField.cs
@@ -0,0 +1,15 @@
+﻿namespace AssemblyLibrary
+{
+    public class AssemblyField
+    {
+        public string fieldType;
+        public string FieldName { get; set; }
+        public string GetFieldsInfo => fieldType + " " + FieldName;
+
+        public AssemblyField(string type, string name)
+        {
+            fieldType = type;
+            FieldName = name;
+        }
+    }
+}
---
+++ AssemblyLibrary/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AssemblyLibrary
+{
+    public class AssemblyInfo
+    {
+        public string AssemblyName { get; set; }
+        public readonly List<AssemblyNamespace> Namespaces;
+
+        public AssemblyInfo(string name, List<AssemblyNamespace> n)
+        {
+            AssemblyName = name;
+            Namespaces = n;
+        }
+    }
+}
