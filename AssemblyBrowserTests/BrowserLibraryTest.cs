49817b1d2f3b15960fa3d0f108203943cc5fbd0f
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-24 00:05:32 +0300

    create tests for browser

--- AssemblyBrowserTests/BrowserLibraryTest.cs
+++ AssemblyBrowserTests/BrowserLibraryTest.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AssemblyLibrary;
-using System.Linq;
 using System.IO;
+using System.Linq;
+using AssemblyLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AssemblyLibTests
-{
-    [TestClass]
-    public class UnitTest1
+namespace AssemblyBrowserTests
+{    [TestClass]
+    public class BrowserTest
     {
         private BrowserLibrary _browserLibrary;
         private AssemblyInfo _assemblyInfo;
@@ -30,42 +29,40 @@ namespace AssemblyLibTests
         [TestMethod]
         public void TypesCount()
         {
-            const int typesCountExpectedFaker = 5;
-            const int typesCountExpectedGens = 7;
 
             var namespaces = _assemblyInfo.Namespaces;
-            var typesFaker = namespaces.First(n => n.NamespaceName == "FakerLibrary").info;
+            var typesFaker = namespaces.First(n => n.NamespaceName == "FakerLibrary").Info;
             var typesCountActualFaker = typesFaker.Count;
 
-            var typesGenerators = namespaces.First(n => n.NamespaceName == "FakerLibrary.Generators.TypesGenerators").info;
+            var typesGenerators = namespaces.First(n => n.NamespaceName == "FakerLibrary.Generators.TypesGenerators").Info;
             var typesCountActualGenerators = typesGenerators.Count;
 
-            Assert.AreEqual(typesCountExpectedFaker, typesCountActualFaker);
-            Assert.AreEqual(typesCountExpectedGens, typesCountActualGenerators);
+            Assert.AreEqual(4, typesCountActualFaker);
+            Assert.AreEqual(7, typesCountActualGenerators);
         }
 
         [TestMethod()]
         public void MethodsCheck()
         {
             var namespaces = _assemblyInfo.Namespaces;
-            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").info.FirstOrDefault(t => t.TypeName == "IGenerator");
-            const string methodname = "CanGenerate";
+            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").Info.FirstOrDefault(t => t.TypeName == "IGenerator");
+            const string methodName = "CanGenerate";
             const string methodSignature = "(Type)";
 
             Assert.IsNotNull(testType);
             Assert.IsTrue(testType.Methods.Count == 2);
-            Assert.IsTrue(testType.Methods.Any(m => m.MethodName == methodname));
-            Assert.IsTrue(testType.Methods.FirstOrDefault(m => m.MethodName == methodname)?.methodSignature == methodSignature);
+            Assert.IsTrue(testType.Methods.Any(m => m.MethodName == methodName));
+            Assert.IsTrue(testType.Methods.FirstOrDefault(m => m.MethodName == methodName)?.methodSignature == methodSignature);
         }
 
         [TestMethod()]
         public void TypeCheck()
         {
             var namespaces = _assemblyInfo.Namespaces;
-            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").info.FirstOrDefault(t => t.TypeName == "Faker");
+            var testType = namespaces.First(n => n.NamespaceName == "FakerLibrary").Info.FirstOrDefault(t => t.TypeName == "Faker");
             const int expectedFieldsAmounts = 4;
             const int expectedPropertiesAmounts = 1;
-            const int expectedMethodsAmounts = 10;
+            const int expectedMethodsAmounts = 7;
 
             Assert.IsNotNull(testType);
             Assert.AreEqual(expectedFieldsAmounts,testType.Fields.Count);
