ab218d481f2a37c1b2a1d04058aa9f3bf0ad75f7
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-10-23 23:45:48 +0300

    create namespace

---
+++ AssemblyLibrary/AssemblyProperty.cs
@@ -0,0 +1,16 @@
+﻿namespace AssemblyLibrary
+{
+    public class AssemblyProperty
+    {
+        public string propertyType;
+        public string propertyName { get; set; }
+        public string GetPropertiesInfo => propertyType + " " + propertyName;
+
+        public AssemblyProperty(string type,string name)
+        {
+            propertyType = type;
+            propertyName = name;
+        }
+
+    }
+}
---
+++ AssemblyLibrary/TypeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace AssemblyLibrary
+{
+    public class TypeInfo
+    {
+        public string TypeName { get; set; }
+        public List<AssemblyField> Fields { get; set; }
+        public List<AssemblyProperty> Properties { get; set; }
+        public List<AssemblyMethod> Methods { get; set; }
+        public TypeInfo(string name)
+        {
+            TypeName = name;
+            Fields = new List<AssemblyField>();
+            Properties = new List<AssemblyProperty>();
+            Methods = new List<AssemblyMethod>();
+        }
+    }
+}
